fbc90a3a00ec80855958dc4f9c95b40e7c4656c1
ngabonzizaguy <ngabonzizaguy1996@gmail.com>
2022-03-28 13:16:28 +0200

    algoexpert challenge

---
+++ nonConstructible.java
@@ -0,0 +1,5 @@
+public class nonConstructible {
+    public static void main(String[] args) {
+        // code
+    }
+}
--- tournamentWinner.java
+++ tournamentWinner.java
@@ -1,44 +1,68 @@
 import java.util.*;
 
 public class tournamentWinner {
-     public static void main(String[] args) {
-        ArrayList<Integer> result = new ArrayList<>();
-        ArrayList<String> team1 = new ArrayList<>();
-        team1.add("Html");
-        team1.add("C#");
-        ArrayList<String> team2 = new ArrayList<>();
-        team2.add("C#");
-        team2.add("Python");
-        ArrayList<String> team3 = new ArrayList<>();
-        team3.add("Python");
-        team3.add("Html");
+    public static void main(String[] args) {
+        ArrayList<String> cpt1 = new ArrayList<>(Arrays.asList("Html", "C#"));
+        ArrayList<String> cpt2 = new ArrayList<>(Arrays.asList("C#", "Python"));
+        ArrayList<String> cpt3 = new ArrayList<>(Arrays.asList("Python", "HTml"));
+        ArrayList<Integer> results = new ArrayList<>(Arrays.asList(0, 0, 1));
+        HashMap<String, Integer> scores = new HashMap<>();
+        ArrayList<ArrayList<String>> competitions = new ArrayList<>(Arrays.asList(cpt1, cpt2, cpt3));
+        ArrayList<String> competition = new ArrayList<>();
 
-        var competitions = new ArrayList<ArrayList<String>>();
-        competitions.add(team1);
-        competitions.add(team2);
-        competitions.add(team3);
+        // algoExpert code
+        String currentTeam = "";
+        String bestTeam = "";
+		scores.put(currentTeam, 0);
 
-        result.add(0);
-        result.add(0);
-        result.add(1);
+        for(int i = 0; i < competitions.size(); i++) {
+            competition = competitions.get(i);
+            int result = results.get(i);
 
-        for(Integer r: result) {
-            System.out.println(r*2);
+            String hometeam = competition.get(0);
+            String awayteam = competition.get(1);
+
+            bestTeam = (result == 1) ? hometeam : awayteam;
+
+            updateScores(bestTeam, 3, scores);
+
+            if(scores.get(bestTeam) > scores.get(currentTeam)) {
+                currentTeam = bestTeam;
+            }
         }
+
+        System.out.println("Winning Team: "+currentTeam);
+		
+    }
+
+    public static void updateScores(String team, int points, HashMap<String, Integer> scores) {
+        if(!scores.containsKey(team)) {
+            scores.put(team, 0);
+        }
+        scores.put(team, scores.get(team) + points);
     }
 }
 
-/**
- * => competitions [
- *      ["Html", "C#"], 
- *      ["C#", "Python"], 
- *      ["Python", "Html"]
- * ]
- * 
- * => results[0, 0, 1]
- * => Winner: "Python" - 6 points (3+3)
- * 
- * ArrayList<ArrayList<String>> competitions, ArrayList<Integer> results
- * private homeTeam = 1;
- * private awayTeam = 0;
- */
+// my_code:
+/** 
+for(int i = 0; i < competitions.size(); i++) {
+    competition = competitions.get(i);
+    int result = results.get(i);
+    int points = 3;
+
+    String homeTeam = competition.get(0);
+    String awayTeam = competition.get(1);
+    
+    if(result == 1) {
+        currentTeam = homeTeam;
+        if(scores.containsKey(currentTeam)) {
+            scores.put(currentTeam, scores.get(currentTeam) + points);
+        } else {
+            scores.put(currentTeam, 0);
+        }
+    } else {
+        currentTeam = awayTeam;
+        scores.put(currentTeam, 0);
+    }
+}
+*/
