834797cb8478074a1f87867ca42189ceb24ffeae
ngabonzizaguy <ngabonzizaguy1996@gmail.com>
2022-03-29 16:25:10 +0200

    non-constructible change

--- nonConstructible.java
+++ nonConstructible.java
@@ -4,16 +4,21 @@ public class nonConstructible {
     public static void main(String[] args) {
         // code
         int[] coins = {5, 7, 1, 1, 2, 3, 22};
+        
+        System.out.println(getChange(coins));
+    }
+
+    public static int getChange(int[] coins) {
         Arrays.sort(coins);
         int change = 0;
 
         for(int c: coins) {
-            if(c > change + ) {
-                change += 1;
+            if(c > change + 1) {
+                return change + 1;
             }
             change += c;
         }
 
-        System.out.println("Change: "+(change + 1));
+        return change + 1;
     }
 }
