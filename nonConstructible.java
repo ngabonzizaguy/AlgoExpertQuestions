0e7c438f808dff54f064e2c82175166b56588aae
ngabonzizaguy <ngabonzizaguy1996@gmail.com>
2022-03-29 16:11:42 +0200

    set approach

--- nonConstructible.java
+++ nonConstructible.java
@@ -1,5 +1,19 @@
+import java.util.*;
+
 public class nonConstructible {
     public static void main(String[] args) {
         // code
+        int[] coins = {5, 7, 1, 1, 2, 3, 22};
+        Arrays.sort(coins);
+        int change = 0;
+
+        for(int c: coins) {
+            if(c > change + ) {
+                change += 1;
+            }
+            change += c;
+        }
+
+        System.out.println("Change: "+(change + 1));
     }
 }
